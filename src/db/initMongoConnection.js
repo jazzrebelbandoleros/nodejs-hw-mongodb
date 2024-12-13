18c5ae25dc410b4fe54d547c27f9695c633b335b
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-12-13 16:05:19 +0300

    change all

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
-dotenv.config();
+import { env } from '../utils/env.js';
 
-export const initMongoConnection = async () => {
+export const initMongoDB = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
