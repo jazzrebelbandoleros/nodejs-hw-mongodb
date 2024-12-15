f926016892665b04deb620a440cb00107e0afc92
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-12-15 20:29:47 +0300

    change all

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,27 +1,19 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const pwd = process.env.MONGODB_PASSWORD;
-    const url = process.env.MONGODB_URL;
-    const db = process.env.MONGODB_DB;
+    const user = env('MONGODB_USER');
+    const password = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
-    const uri = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
-
-    console.log('Attempting to connect to MongoDB:', uri);
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log('MongoDB connection successful');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    throw error;
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (err) {
+    console.log('Error while setting up mongo connection', err);
+    throw err;
   }
 };
