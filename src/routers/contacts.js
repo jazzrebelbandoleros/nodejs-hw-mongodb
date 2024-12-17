2c0254c0e04211e439d3be71f8351f4c26dab2a4
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-12-17 22:50:29 +0300

    change all

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,26 +1,22 @@
 import { Router } from 'express';
 import {
-  getContactByIdController,
   getContactsController,
+  getContactsByIdController,
   createContactController,
-  deleteContactController,
-  upsertContactController,
   patchContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
-
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
 router.post('/contacts', ctrlWrapper(createContactController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
-
-router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
-
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
