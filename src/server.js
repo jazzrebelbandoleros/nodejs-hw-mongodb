715fdc57216566ab77f68bb0a657b2a067d73827
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-12-17 12:12:51 +0300

    change all

--- src/server.js
+++ src/server.js
@@ -1,20 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import dotenv from 'dotenv';
+import { getAllContacts, getContactsById } from './services/contacts.js';
 import { getEnvVar } from './utils/getEnvVar.js';
-import contactsRouter from './routers/contacts.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-dotenv.config();
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
-  app.use(cors());
   app.use(express.json());
+  app.use(cors());
   app.use(
     pino({
       transport: {
@@ -23,12 +19,42 @@ export const setupServer = () => {
     }),
   );
 
-  app.use(contactsRouter);
+  app.get('/contacts', async (req, res) => {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
 
-  app.use('*', notFoundHandler);
+  app.get('/contacts/:contactId', async (req, res, next) => {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  });
 
-  app.use(errorHandler);
-
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+  });
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
