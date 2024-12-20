2fe155e632c12e0d38743f819f67106de035e902
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-12-20 14:30:06 +0300

    change all

--- src/server.js
+++ src/server.js
@@ -1,33 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
 import dotenv from 'dotenv';
-import { getEnvVar } from './utils/getEnvVar.js';
+import { logger } from './middlewars/logger.js';
 import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewars/errorHandler.js';
 import { notFoundHandler } from './middlewars/notFoundHandler.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+dotenv.config();
+const PORT = Number(process.env.PORT) || 3000;
 
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
   app.use(cors());
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
+  app.use(express.json());
+  app.use(logger);
   app.use(contactsRouter);
-
   app.use('*', notFoundHandler);
-
   app.use(errorHandler);
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
