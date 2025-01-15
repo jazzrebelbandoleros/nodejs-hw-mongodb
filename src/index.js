45c025f046dccd0b7fa65a39323abc36b4ed42ce
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2025-01-15 19:10:03 +0300

    change all

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { createDirIfNotExists } from './utils/createDirlfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const bootstrap = async () => {
