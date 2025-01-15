7e0c44503723950f8b66f31e6e923374fb31d93e
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2025-01-15 19:21:45 +0300

    change all

---
+++ src/validation/auth.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+import { emailRegexp } from '../constants/index.js';
+
+export const registerUserSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const requestResetEmailSchema = Joi.object({
+  email: Joi.string().email().required(),
+});
+
+export const resetPasswordSchema = Joi.object({
+  password: Joi.string().required(),
+  token: Joi.string().required(),
+});
